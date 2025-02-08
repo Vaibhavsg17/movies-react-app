4478550fc284984e7c073318d36af5e7e9790644
Vaibhavsg17 <vaibhav.s221064102@vcet.edu.in>
2025-02-08 21:16:36 +0530

    added appwrite backend

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-const Search = ({ searchTerm, setsearchTerm }) => {
+const Search = ({ searchTerm, setSearchTerm }) => {
     return (
-        <div className='search'>
+        <div className="search">
             <div>
                 <img src="search.svg" alt="search" />
+
                 <input
                     type="text"
                     placeholder="Search through thousands of movies"
                     value={searchTerm}
-                    onChange={(e) => setsearchTerm(e.target.value)}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
             </div>
         </div>
     )
 }
-
 export default Search
