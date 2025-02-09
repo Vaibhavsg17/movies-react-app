da0e259467f003af2b4ab342579c87bcb9a88639
Vaibhavsg17 <vaibhav.s221064102@vcet.edu.in>
2025-02-09 14:21:39 +0530

    Fix case sensitivity issue 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
-import Search from "./components/Search.jsx";
-import Spinner from './components/spinner.jsx';
-import MovieCard from './components/MovieCard.jsx';
+import Search from "./Components/Search.jsx";
+import Spinner from './Components/Spinner.jsx';
+import MovieCard from './Components/MovieCard.jsx';
 import { useDebounce } from 'react-use';
 import { getTrendingMovies, updateSearchCount } from './appwrite.js';
 
